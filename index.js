bb5ea263f9b2d7cb404254279a01e3b59606793c
Austin Blake <ummwhere@hotmail.com>
2017-11-29 21:48:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  var e = kittens.pop();
+  return e;
+}
+
+function destructivelyRemoveFirstKitten() {
+  var e = kittens.shift();
+  return e;
+}
